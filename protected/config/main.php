13e81484fd9698d4c3c4c3f9b661584697d7ce0b
Roberth <roberth.riera@insignia.com.ve>
2016-09-05 20:08:50 -0400

    Configuracion de la app

--- protected/config/main.php
+++ protected/config/main.php
@@ -1,16 +1,67 @@
 <?php
 
+if(($_SERVER['SERVER_NAME'] == 'localhost') || ($_SERVER['SERVER_NAME'] == '72.14.188.47')){
+
+	//Servidores
+	$host_F1 = "200.109.237.18";
+	$host_F2 = "200.109.237.18";
+	$host_Hostgator = "200.109.237.18";
+
+	//Datos F1
+	$user_F1 = 'roberth.riera';
+	$pwd_F1 = 'WPjvzyb7na';
+
+	//Datos F2
+	$user_F2 = 'roberth.riera';
+	$pwd_F2 = 'WPjvzyb7na';
+
+	//Datos insignia_masivo_premium
+	$user_F2_mp = 'roberth.riera';
+	$pwd_F2_mp = 'WPjvzyb7na';
+
+	//Datos Hostgator
+	$user_Hostgator = 'roberth.riera';
+	$pwd_Hostgator = 'WPjvzyb7na';
+} else
+	{
+		//Servidores
+		$host_F1 = "72.233.82.70";
+		$host_F2 = "72.232.85.20";
+		$host_Hostgator = "www.insigniamobile.com.ve";
+
+		//Datos F1
+		$user_F1 = 'rcma';
+		$pwd_F1 = 'rcma';
+
+		//Datos F2
+		$user_F2 = 'rcma';
+		$pwd_F2 = 'rcma';
+
+		//Datos insignia_masivo_premium
+		$user_F2_mp = 'envios_mt';
+		$pwd_F2_mp = 'AfnEn1istb';
+
+		//Datos Hostgator
+		$user_Hostgator = 'insignia_envios';
+		$pwd_Hostgator = 'fkhXOF3UXhIMC2015';
+	}
+
 // uncomment the following to define a path alias
 // Yii::setPathOfAlias('local','path/to/local-folder');
 
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
+Yii::setPathOfAlias('booster', dirname(__FILE__).'/../extensions/booster');
+
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Broadcasting Plus',
+	'timeZone' => 'America/Caracas',
+    'language'=>'es',
+    //'theme'=>'blackboot',
 
 	// preloading 'log' component
-	'preload'=>array('log'),
+	'preload'=>array('log', 'booster'),
 
 	// autoloading model and component classes
 	'import'=>array(
@@ -20,14 +71,13 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
+		
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
 			'password'=>'Enter Your Password Here',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
 	),
 
 	// application components
@@ -38,20 +88,91 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 
+		'booster' => array(
+    		'class' => 'booster.components.Booster',
+		),
+
 		// uncomment the following to enable URLs in path-format
-		/*
-		'urlManager'=>array(
+		
+		/*'urlManager'=>array(
 			'urlFormat'=>'path',
+			'showScriptName'=>false,
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
+		),*/
+
+		'urlManager'=>array(
+			'urlFormat'=>'path',
+			
+			'showScriptName'=>false,
+			'rules'=>array(
+				'' => 'site/index', // normal URL rules
+		                   array( // your custom URL handler
+			  					'class' => 'application.components.CustomUrlRule',
+			  				),       
+	  			
+			),
 		),
-		*/
+		
 
 		// database settings are configured in database.php
-		'db'=>require(dirname(__FILE__).'/database.php'),
+		//'db'=>require(dirname(__FILE__).'/database.php'),
+
+		'db'=>array(
+			'connectionString' => 'mysql:host='.$host_F2.';dbname=insignia_masivo',
+			'emulatePrepare' => true,
+			'username' => $user_F2,
+			'password' => $pwd_F2,
+			'charset' => 'utf8',
+		),
+
+		'db_masivo_premium'=>array(
+			'class' => 'CDbConnection',
+			'connectionString' => 'mysql:host='.$host_F2.';dbname=insignia_masivo_premium',
+			'emulatePrepare' => true,
+			'username' => $user_F2_mp,
+			'password' => $pwd_F2_mp,
+			'charset' => 'utf8',
+		),
+
+		'db_insignia_alarmas'=>array(
+			'class' => 'CDbConnection',
+			'connectionString' => 'mysql:host='.$host_Hostgator.';dbname=insignia_alarmas',
+			'emulatePrepare' => true,
+			'username' => $user_Hostgator,
+			'password' => $pwd_Hostgator,
+			'charset' => 'utf8',
+		),
+
+		'db_sms'=>array(
+			'class' => 'CDbConnection',
+			'connectionString' => 'mysql:host='.$host_F1.';dbname=sms',
+			'emulatePrepare' => true,
+			'username' => $user_F1,
+			'password' => $pwd_F1,
+			'charset' => 'utf8',
+		),
+
+		'db_insignia_admin'=>array(
+			'class' => 'CDbConnection',
+			'connectionString' => 'mysql:host='.$host_F1.';dbname=insignia_admin',
+			'emulatePrepare' => true,
+			'username' => $user_F1,
+			'password' => $pwd_F1,
+			'charset' => 'utf8',
+		),
+
+		'db_supervision_modems'=>array(
+			'class' => 'CDbConnection',
+			'connectionString' => 'mysql:host='.$host_F2.';dbname=supervision_modems',
+			'emulatePrepare' => true,
+			'username' => $user_F2,
+			'password' => $pwd_F2,
+			'charset' => 'utf8',
+		),
 
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -20,6 +20,48 @@
 
 <body>
 
+<div>
+	<?php
+		$this->widget(
+	    'booster.widgets.TbNavbar',
+	    array(
+	        'type' => 'inverse',
+	        'brand' => Yii::app()->name,//.CHtml::image(Yii::app()->getBaseUrl().'/images/logoInsig3.png'),
+	        'brandUrl' => '#',
+	        'collapse' => true, // requires bootstrap-responsive.css
+	        'fixed' => 'top',
+	        'fluid' => true,
+	        'items' => array(
+	            array(
+	                'class' => 'booster.widgets.TbMenu',
+	                'type'  => 'navbar',
+	                'htmlOptions' => array('class' => 'pull-right '),
+	                'items' =>  array(
+		                    array('label' => 'Inicio', 'icon'=>'glyphicon glyphicon-home','url' => Yii::app()->createUrl('site/index'), 'active' => true, 'visible'=>!Yii::app()->user->isGuest),
+		                    array('label' => 'CPEI', 'url' => '#', 'icon'=>'glyphicon glyphicon-tag','visible'=>!Yii::app()->user->isGuest),
+		                    array('label' => 'Broadcasting Premium', 'icon'=>'glyphicon glyphicon-tags','url' => '#', 'visible'=>!Yii::app()->user->isGuest),
+		                    array('label'=>'Iniciar Sesión', 'url'=>Yii::app()->createUrl('site/login'), 'visible'=>Yii::app()->user->isGuest),
+		                    array(
+		                        'label' => Yii::app()->user->name,
+		                        'url' => '#',
+		                        'icon' => 'glyphicon glyphicon-user',
+		                        'visible'=>!Yii::app()->user->isGuest,
+		                        'items' => array(
+		                            array(
+		                            	'label'=>'Cerrar Sesión',
+		                            	'icon'=>'glyphicon glyphicon-off', 
+		                            	'url'=>Yii::app()->createUrl('/site/logout'), 
+		                        	)
+		                   	 	),
+		                	),
+	    				),
+	        		),
+	    		)
+	        )
+		);
+	?>
+</div>
+
 <div class="container" id="page">
 
 	<div id="header">
@@ -29,10 +71,10 @@
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+				array('label'=>'Home', 'url'=>Yii::app()->createUrl('site/index')),
+				array('label'=>'About', 'url'=>Yii::app()->createUrl('site/page')),
+				array('label'=>'Contact', 'url'=>Yii::app()->createUrl('site/contact')),
+				array('label'=>'Login', 'url'=>Yii::app()->createUrl('site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
 		)); ?>
